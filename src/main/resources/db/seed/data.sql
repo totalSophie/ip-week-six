2f830da6955a0532190d59c79c26dbddfd3cf431
Sophia Chisiya <totalsophie@gmail.com>
2023-11-29 15:02:04 +0300

    Update resources folder

---
+++ src/main/resources/db/seed/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO animals (name) VALUES ('Lion'), ('Elephant'), ('Giraffe');
+INSERT INTO endangered_animals (name, health, age) VALUES ('Panda', 'healthy', 'adult'), ('Tiger', 'okay', 'young');
