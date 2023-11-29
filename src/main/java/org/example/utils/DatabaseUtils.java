481d90788d57071f0bd24f9d853f8393b2947f85
Sophia Chisiya <totalsophie@gmail.com>
2023-11-29 14:40:28 +0300

    Add database Utils

--- src/main/java/org/example/utils/DatabaseUtils.java
+++ src/main/java/org/example/utils/DatabaseUtils.java
@@ -1,4 +1,49 @@
 package org.example.utils;
 
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.config.JdbiConfig;
+import org.jdbi.v3.core.spi.JdbiPlugin;
+
 public class DatabaseUtils {
+    private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/wildlife_tracker";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "password";
+
+    private static final Jdbi jdbi = initJdbi();
+
+    private static Jdbi initJdbi() {
+        return Jdbi.create(DATABASE_URL, USER, PASSWORD)
+                .installPlugin(new AnimalPlugin())  // Add additional plugins if needed
+                .installPlugin(new EndangeredAnimalPlugin())
+                .installPlugin(new SightingPlugin());
+    }
+
+    public static Jdbi getJdbi() {
+        return jdbi;
+    }
+
+    public static <T> T getDao(Class<T> daoClass) {
+        return getJdbi().onDemand(daoClass);
+    }
+
+    private static class AnimalPlugin implements JdbiPlugin {
+        @Override
+        public void customizeJdbi(JdbiConfig config) {
+            // Customize Jdbi for the Animal class if needed
+        }
+    }
+
+    private static class EndangeredAnimalPlugin implements JdbiPlugin {
+        @Override
+        public void customizeJdbi(JdbiConfig config) {
+            // Customize Jdbi for the EndangeredAnimal class if needed
+        }
+    }
+
+    private static class SightingPlugin implements JdbiPlugin {
+        @Override
+        public void customizeJdbi(JdbiConfig config) {
+            // Customize Jdbi for the Sighting class if needed
+        }
+    }
 }
