3328b0fa89bba6d59fbb4e67a8b525a1735a4fd6
Sophia Chisiya <totalsophie@gmail.com>
2023-12-07 08:26:07 +0300

    Update App class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,26 +1,14 @@
 package org.example;
 
-import org.example.controllers.HomeController;
-import org.example.services.WildlifeService;
-
-import static spark.Spark.*;
+import org.example.database.Db;
+import org.example.database.Seeder;
+import org.example.utils.Router;
+import org.sql2o.Connection;
 
 public class App {
     public static void main(String[] args) {
-        // Set up database and services
-        WildlifeService wildlifeService = new WildlifeService();
-
-        // Set up static files (CSS, JS, etc.)
-        //staticFiles.location("/public");
-
-        // Set up routes
-        HomeController homeController = new HomeController(wildlifeService);
-
-        get("/", homeController.getIndex);
-        post("/submit_sighting", homeController.submitSighting);
-        get("/animals", homeController.getAllAnimals);
-
-        // Add other routes as needed
-
+        Connection connection = new Db().connect();
+        Seeder.seed(connection);
+        Router.route(connection);
     }
 }
